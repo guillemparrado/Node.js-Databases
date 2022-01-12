5cd93b70d115bde017dc9c84e2c4041dec5c4e4f
Guillem Parrado <guillempado@gmail.com>
2022-01-12 16:16:25 +0100

    Pizzeria - schema inserts i verificacions completat

--- Pizzeria_inserts_verificacions.sql
+++ Pizzeria_inserts_verificacions.sql
@@ -1,13 +1,106 @@
+USE pizzeria;
+
 -- INSERTS
 
+INSERT INTO province
+VALUES (1, 'Barcelona');
+
+INSERT INTO locality
+VALUES (1, 'Barcelona', 1);
+
+INSERT INTO locality
+VALUES (2, 'Sabadell', 1);
+
+INSERT INTO store
+VALUES (1, 'Torrent de la vil·la 12', '08080', 1, 1);
+
+INSERT INTO store
+VALUES (2, 'Carrer de la fàbrica 123', '08202', 2, 1);
+
+INSERT INTO client
+VALUES (1, 'Omar', 'Olmedo Ferrer', 'Gran Via 123', 08080, 1, 1, '555-555-555');
+
+INSERT INTO employee
+VALUES (1, 1, 'Agustí', 'Pérez Simó', '12345678X', '555-555-555', 'cooker');
+
+INSERT INTO employee
+VALUES (2, 1, 'Sònia', 'Domínguez Guatlla', '98765432X', '666-666-666', 'delivery');
+
+INSERT INTO pizza_category
+VALUES (1, 'Clàssica');
+
+INSERT INTO pizza_category
+VALUES (2, 'Deluxe');
+
+-- Imatge codificada a base64 a: https://onlinepngtools.com/convert-png-to-base64
+-- Entenc que la codificació la faríem en node abans de guardar
+-- També podria haver fet servir LOAD_FILE però no accepta urls per obtenir les imatges, només paths absoluts del filesystem i per replicar en test era una mica incòmode (caldria copiar carpeta d'imatges manualment a destinació objectiva perquè els inserts funcionessin en una altra màquina).
+-- Imatges originals: https://www.dominospizza.es/images/CarbonaraDelivery_N2200923_0_ES.png, https://www.dominospizza.es/images/CremozzaBBQDelivery_Z2200923_0_ES.png.
+-- OBSERVACIÓ: He acabat resumint els blobs a una línia per comoditat del test ja que no l'haurem d'interpretar i era massa llarg
+
+INSERT INTO product
+VALUES (1, 'pizza', 'Carbonara', 'Crema fresca, Mozzarella, Bacon, Xampinyons i Ceba',
+        'TLdnH2ATSTl68YkTyMJwPLnGbz1xxKgLuJP...NeUWOUVIkfj/+dveHT+tUutfdtWVqr7fma5xHX9Ns1==', 14.25, 1);
+
+-- Mateix blob resumit per simplificar
+INSERT INTO product
+VALUES (2, 'pizza', 'Cremozza BBQ', 'Crema fresca, Mozzarella, Bacon, Pollastre a la graella,Ceba i Salsa barbacoa',
+        'TLdnH2ATSTl68YkTyMJwPLnGbz1xxKgLuJP...NeUWOUVIkfj/+dveHT+tUutfdtWVqr7fma5xHX9Ns1==', 21.75, 2);
+
+INSERT INTO product
+VALUES (3, 'beverage', 'Aigua', 'Mineral Natural',
+        'TLdnH2ATSTl68YkTyMJwPLnGbz1xxKgLuJP...NeUWOUVIkfj/+dveHT+tUutfdtWVqr7fma5xHX9Ns1==', 1, NULL);
+
+INSERT INTO product
+VALUES (4, 'beverage', 'Cola-Cola', 'Llauna',
+        'TLdnH2ATSTl68YkTyMJwPLnGbz1xxKgLuJP...NeUWOUVIkfj/+dveHT+tUutfdtWVqr7fma5xHX9Ns1==', 1.5, NULL);
+
+INSERT INTO product
+VALUES (5, 'beverage', 'Estrella Damm', 'Llauna',
+        'TLdnH2ATSTl68YkTyMJwPLnGbz1xxKgLuJP...NeUWOUVIkfj/+dveHT+tUutfdtWVqr7fma5xHX9Ns1==', 1.5, NULL);
 
 
+-- Sale d'una pizza carbonara i una cervesa a botiga barcelona, entrega a domicili efectuada per l'empleada 2
 
+INSERT INTO home_delivery
+VALUES (1, 2, '2020-01-01 20:45');
+
+INSERT INTO sale
+VALUES (1, '2020-01-01 20:15', 1, 1, 15.75, 1);
+
+INSERT INTO sale_line
+VALUES (NULL, 1, 1, 1, 14.25);
+
+INSERT INTO sale_line
+VALUES (NULL, 1, 5, 1, 1.5);
+
+-- Sale de 6 Coca-coles a botiga barcelona, entrega a botiga
+
+INSERT INTO sale
+VALUES (2, '2021-01-03 18:12', NULL, NULL, 9, 1);
+
+INSERT INTO sale_line
+VALUES (NULL, 2, 4, 6, 9);
 
 -- VERIFICACIONS
 
 -- Llista quants productes del tipus 'begudes' s'han venut en una determinada localitat
+-- Resultat: 7 == 1 cervesa i 6 coca-coles
 
-
+SELECT SUM(quantity)
+FROM sale s
+         JOIN sale_line sl ON s.id = sl.sale
+         JOIN product p ON sl.product = p.id
+WHERE p.type = 'beverage'
+  AND s.store = 1;
 
 -- Llista quantes comandes ha efectuat un determinat empleat
+-- Observació: entenc que l'empleat ha de ser repartidor perquè dels cuiners no en guardem les comandes
+-- Resultat: l'empleada 2 ha efectuat l'entrega d'una comanda
+
+SELECT COUNT(*)
+FROM sale s
+         JOIN home_delivery hd ON s.delivery = hd.id
+WHERE employee = 2
+
+
--- Pizzeria_schema.sql
+++ Pizzeria_schema.sql
@@ -18,12 +18,12 @@ CREATE TABLE locality
 
 -- phone_number és charchar perquè pot tenir guions, parèntesis, etc.
 -- A partir d'aquí poso sempre not null a no ser que s'especifiqui el contrari.
--- Dubte: Cal posar FK a província? no es podria fer un walk des de localitat?
+-- Dubte: Cal posar FK a província? no es podria fer un join des de localitat?
 CREATE TABLE client
 (
     id           INT PRIMARY KEY AUTO_INCREMENT,
     name         VARCHAR(30) NOT NULL,
-    surname      VARCHAR(50) NOT NULL,
+    surnames     VARCHAR(50) NOT NULL,
     address      VARCHAR(50) NOT NULL,
     postal_code  INT(5)      NOT NULL,
     locality     INT         NOT NULL,
@@ -49,10 +49,10 @@ CREATE TABLE employee
     id              INT PRIMARY KEY AUTO_INCREMENT,
     store           INT         NOT NULL,
     name            VARCHAR(30) NOT NULL,
-    surname         VARCHAR(50) NOT NULL,
+    surnames        VARCHAR(50) NOT NULL,
     nif             VARCHAR(9)  NOT NULL,
     phone_number    VARCHAR(20) NOT NULL,
-    job_description ENUM ('cooker', 'delivery_man'),
+    job_description ENUM ('cooker', 'delivery'),
     CONSTRAINT fk_employee_store FOREIGN KEY (store) REFERENCES store (id)
 );
 
@@ -68,23 +68,21 @@ CREATE TABLE home_delivery
 
 -- "order" (comanda en anglès) és sql reserved word, faig servir "sale".
 -- "price" és decimal perquè float pot donar problemes d'exactitut en decimals quan es treballa en moneda. Li he posat 3 dígits de part entera i 2 de decimal.
+-- si delivery és null, l'entrega va ser en botiga, mentre que si apunta a un home_delivery va ser a domicili
+-- "client" pot ser null per compres a botiga per exemple en què no tenim dades de la identitat dels compradors
 -- TODO No se com fer que tingui com a mínim una línia de producte (== mínim un sale_line fk apuntant cada sale perquè pugui existir)
 CREATE TABLE sale
 (
-    id            INT PRIMARY KEY AUTO_INCREMENT,
-    dt            DATETIME      NOT NULL,
-    client        INT           NOT NULL,
-    delivery      ENUM ('home_address', 'store_pickup'),
-    total_price   DECIMAL(5, 2) NOT NULL,
-    store         INT           NOT NULL,
-    home_delivery INT,
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    dt          DATETIME      NOT NULL,
+    client      INT,
+    delivery    INT,
+    total_price DECIMAL(5, 2) NOT NULL,
+    store       INT           NOT NULL,
     CONSTRAINT fk_sale_client FOREIGN KEY (CLIENT) REFERENCES CLIENT (id),
     -- CONSTRAINT check_sale_min_1_product CHECK ( ...? ),
     CONSTRAINT fk_sale_store FOREIGN KEY (store) REFERENCES store (id),
-    CONSTRAINT fk_sale_home_delivery FOREIGN KEY (home_delivery) REFERENCES home_delivery (id),
-    CONSTRAINT check_sale_home_delivery CHECK (
-            (NOT (delivery = 'home_address') AND home_delivery IS NULL) OR
-            (delivery = 'home_address' AND home_delivery IS NOT NULL))
+    CONSTRAINT fk_sale_home_delivery FOREIGN KEY (delivery) REFERENCES home_delivery (id)
 );
 
 CREATE TABLE pizza_category
@@ -92,12 +90,13 @@ CREATE TABLE pizza_category
     id   INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
+
 CREATE TABLE product
 (
     id             INT PRIMARY KEY AUTO_INCREMENT,
     type           ENUM ('pizza', 'burger', 'beverage'),
     name           VARCHAR(30)   NOT NULL,
-    description    VARCHAR(100)  NOT NULL,
+    description    TEXT          NOT NULL,
     image          BLOB          NOT NULL,
     price          DECIMAL(5, 2) NOT NULL,
     pizza_category INT,
