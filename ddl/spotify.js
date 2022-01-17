647c75f17855866f46e6175a0af66437d049727c
Guillem Parrado <guillempado@gmail.com>
2022-01-17 14:49:45 +0100

    spotify.js template

--- ddl/spotify.js
+++ ddl/spotify.js
@@ -1,1 +1,30 @@
-// Placeholder >> L'IDE elimina els arxius de consola buits en reiniciar
+// Arxiu per executar des de la consola (bash): mongosh < spotify.js
+
+use spotify
+db.dropDatabase()
+// show collections
+
+// CREATE COLLECTIONS
+db.createCollection('user');
+// TODO
+
+// MODEL DATA
+
+const user_ids = [ObjectId()];
+// TODO
+
+const users = [
+    {
+        _id: user_ids[0],
+        // TODO
+    }
+]
+
+// INSERTS
+db.user.insertMany(users);
+// TODO
+
+// INDEXES
+// TODO (si cal). Exemple DB anterior: db.video.createIndex({'comments.id': 1});
+
+
